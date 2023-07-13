a919bf7709c1b26a9972b990f3f4b2fa234e27d5
Krishna Venkatramani <krish_venkat11@hotmail.com>
2023-07-13 10:24:01 +0530

    Example Update!

---
+++ Example/Pods/Target Support Files/Pods-UIKLayout_Tests/Pods-UIKLayout_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIKLayout_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UIKLayout_Tests
+@end
---
+++ Example/Pods/Target Support Files/UIKLayout/UIKLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIKLayout : NSObject
+@end
+@implementation PodsDummy_UIKLayout
+@end
